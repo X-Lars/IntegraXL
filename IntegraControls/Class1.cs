62df478787df68d85c20bad1b7aadc2f036db06a
XLars <xlars@xlars.com>
2020-11-20 08:27:29 +0100

    Initial Project Structure

---
+++ IntegraControls/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegraControls
+{
+    public class Class1
+    {
+    }
+}
