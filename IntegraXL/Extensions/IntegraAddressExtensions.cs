65a82faebbf6cc7b87186df1103bcf2739f27030
XLars <xlars@xlars.com>
2022-02-10 09:32:50 +0100

    Changed Accessebility - Code Cleanup

--- IntegraXL/Extensions/IntegraAddressExtensions.cs
+++ IntegraXL/Extensions/IntegraAddressExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using IntegraXL.Core;
+using System.Diagnostics;
 
 namespace IntegraXL.Extensions
 {
@@ -11,12 +12,14 @@ namespace IntegraXL.Extensions
         /// Gets wheter the current address is within the specified address range.
         /// </summary>
         /// <param name="instance">The current address.</param>
-        /// <param name="min">The address range lower limit.</param>
-        /// <param name="max">The address range upper limit.</param>
+        /// <param name="min">The address range inclusive lower limit.</param>
+        /// <param name="max">The address range inclusive upper limit.</param>
         /// <returns>True if the current address lies within range of the provided addresses.</returns>
-        public static bool InRange(this IntegraAddress instance, IntegraAddress min, IntegraAddress max)
+        internal static bool InRange(this IntegraAddress instance, IntegraAddress min, IntegraAddress max)
         {
-            return min > max ? instance >= max && instance <= min : instance >= min && instance <= max;
+            Debug.Assert(max >= min);
+
+            return instance >= min && instance <= max;
         }
 
         /// <summary>
@@ -24,8 +27,10 @@ namespace IntegraXL.Extensions
         /// </summary>
         /// <param name="instance">The current address.</param>
         /// <returns>The studio set part.</returns>
-        public static Parts GetStudioSetPart(this IntegraAddress instance)
+        internal static Parts GetStudioSetPart(this IntegraAddress instance)
         {
+            Debug.Assert(instance.InRange(0x18001000, 0x18005F00));
+
             return (Parts)((instance & 0x00000F00) >> 8);
         }
 
@@ -34,15 +39,17 @@ namespace IntegraXL.Extensions
         /// </summary>
         /// <param name="instance">The current address.</param>
         /// <returns>The temporary tone part.</returns>
-        public static Parts GetTemporaryTonePart(this IntegraAddress instance)
+        internal static Parts GetTemporaryTonePart(this IntegraAddress instance)
         {
+            Debug.Assert(instance.InRange(0x19000000, 0x1D000000));
+
             // MSB: 0x19, 0x19, 0x19, 0x19, 0x20, ...
             // LSB: 0x00, 0x20, 0x40, 0x60, 0x00, ...
 
-            int msb = (instance[0] - 0x19) << 2; // << 2 equals multiplication by 4
+            int msb = (instance[0] - 0x19) * 4;
             int lsb = (instance[1] / 0x20);
 
-            return (Parts)(lsb + msb);
+            return (Parts)(msb + lsb);
         }
     }
 }
