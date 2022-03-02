a78cbf6c0c908cfbe53fdfea96eb45fa4cfa0f3d
XLars <xlars@xlars.com>
2022-03-02 21:55:48 +0100

    Added Basic file Setup

---
+++ IntegraXL/File/FileManager.cs
@@ -0,0 +1,73 @@
+﻿
+namespace IntegraXL.File
+{
+    public static class FileManager
+    {
+        public static StudioSetFile Create()
+        {
+            return new StudioSetFile();
+        }
+
+
+        public static StudioSetFile Load(byte[] data)
+        {
+            StudioSetFile file = new();
+
+            MemoryStream stream = new(data);
+            BinaryReader reader = new(stream);
+
+            file.Common = reader.ReadBytes(file.Common.Length);
+            file.CommonChorus = reader.ReadBytes(file.CommonChorus.Length);
+            file.CommonReverb = reader.ReadBytes(file.CommonReverb.Length);
+            file.MotionalSurround = reader.ReadBytes(file.MotionalSurround.Length);
+            file.MasterEQ = reader.ReadBytes(file.MasterEQ.Length);
+
+            for (int i = 0; i < 16; i++)
+            {
+                file.Midis[i] = reader.ReadBytes(file.Midis[i].Length);
+            }
+
+            for (int i = 0; i < 16; i++)
+            {
+                file.Parts[i] = reader.ReadBytes(file.Parts[i].Length);
+            }
+
+            for (int i = 0; i < 16; i++)
+            {
+                file.PartEQs[i] = reader.ReadBytes(file.PartEQs[i].Length);
+            }
+
+            return file;
+        }
+
+
+        public static byte[] Save(StudioSetFile file)
+        {
+            MemoryStream stream = new();
+            BinaryWriter writer = new(stream);
+
+            writer.Write(file.Common);
+            writer.Write(file.CommonChorus);
+            writer.Write(file.CommonReverb);
+            writer.Write(file.MotionalSurround);
+            writer.Write(file.MasterEQ);
+
+            for (int i = 0; i < 16; i++)
+            {
+                writer.Write(file.Midis[i]);
+            }
+
+            for (int i = 0; i < 16; i++)
+            {
+                writer.Write(file.Parts[i]);
+            }
+
+            for (int i = 0; i < 16; i++)
+            {
+                writer.Write(file.PartEQs[i]);
+            }
+
+            return stream.ToArray();
+        }
+    }
+}
---
+++ IntegraXL/File/FileTypes.cs
@@ -0,0 +1,55 @@
+﻿using IntegraXL.Core;
+
+namespace IntegraXL.File
+{
+
+    public struct TemporaryToneFile
+    {
+        public string Name;
+        public byte[] Model;
+    }
+
+    public struct StudioSetFile
+    {
+        public StudioSetFile()
+        {
+            Common = new byte[84];
+            CommonChorus = new byte[84];
+            CommonReverb = new byte[99];
+            MotionalSurround = new byte[16];
+            MasterEQ = new byte[7];
+
+            Midis = new byte[IntegraConstants.PART_COUNT][];
+
+            for (int i = 0; i < 16; i++)
+            {
+                Midis[i] = new byte[1];
+            }
+
+            Parts = new byte[IntegraConstants.PART_COUNT][];
+
+            for (int i = 0; i < 16; i++)
+            {
+                Parts[i] = new byte[77];
+            }
+
+            PartEQs = new byte[IntegraConstants.PART_COUNT][];
+
+            for (int i = 0; i < 16; i++)
+            {
+                PartEQs[i] = new byte[8];
+            }
+        }
+
+        public byte[] Common;
+        public byte[] CommonChorus;
+        public byte[] CommonReverb;
+        public byte[] MotionalSurround;
+        public byte[] MasterEQ;
+        public byte[][] Midis;
+        public byte[][] Parts;
+        public byte[][] PartEQs;
+    }
+
+    
+}
